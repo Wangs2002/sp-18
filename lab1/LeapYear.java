aab2d821ebb1ff395c4b533d38ea4afb892cb11e
Wangs <Wangs131@outlook.com>
2023-09-24 16:49:45 +0800

    LeapYear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -14,6 +14,16 @@ public class LeapYear {
         }
     }
 
+    public static boolean isLeapYear(int year){
+	    /* if year is divisible by 400
+	     * if year is divisible by 4 and not by 100
+	     */
+	if(year % 4 ==0 && year % 100 != 0 || year % 400 == 0){
+	return true;
+	}
+	else return false;
+    }
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
