440a9023e8afeccf3b219cc91af0e3946c592542
Wangs <Wangs131@outlook.com>
2023-09-24 11:33:56 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,10 @@
+public class HelloNumbers{
+	public static void main(String[] args){
+		int result = 0, i = 0;
+		while(result < 45){
+			result = result + i;
+			i++;
+			System.out.print(result + " ");		
+		}
+	}
+}
